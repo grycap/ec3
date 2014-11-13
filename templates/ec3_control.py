bfacfda39c258505f4a300e337cded13fd231b6b
Eloy Romero <elroal@upvnet.upv.es>
2014-11-13 10:35:26 +0100

    Many changes in ec3_control: - Class Control has the program data and the functions that can be overloaded by pluggins. - Various pluggins can be loaded. - Improved the output log. - Added support for more than one system: ec3_max_instances and ec3_if_fail. - Added support for queues: it tries to select a system named for the queue

--- templates/ec3_control.py
+++ templates/ec3_control.py
@@ -2,135 +2,178 @@
 
 # Experimental control
 
-import sys, logging, xmlrpclib, argparse, time, imp, threading, subprocess
+import sys, logging, xmlrpclib, argparse, time, imp, threading, subprocess, socket
 from IM.auth import Authentication
 from IM.radl.radl_parse import parse_radl
 from IM.radl.radl import RADL, system, configure, deploy
 
 AUTH = LAUNCH_RADL = LIMITS = DESTROY_INTERVAL = DELAY = None
-get_meta_state = get_queued_jobs = enable_node = disable_node = None
 logger = None
 
-IM_SERVER = xmlrpclib.ServerProxy("http://localhost:8899", allow_none=True)
-    
-# vmids: dict[str: hostname] = system
-def get_vmids(vmids):
-    success, s = IM_SERVER.GetInfrastructureInfo(0, AUTH)
-    if not success: Exception("Error calling GetInfrastructureInfo: %s" % s)
-    vmids.clear()
-    for vmid in s["vm_list"]:
-        get_vmid(vmids, vmid)
+def san(v, maxlen=600):
+    cut = lambda r: r if len(r) < maxlen else r[0:maxlen-6] + "..." + r[-3:-1]
+    return (cut(v.decode("string_escape")) if isinstance(v, str) else
+            "[%s]" % ", ".join(map(lambda s: san(s, maxlen), v)) if isinstance(v, (set, list, tuple)) else
+            "{%s}" % ", ".join([ "%s: %s" % (a, san(b, maxlen)) for a,b in v.items() ]) if isinstance(v, dict) else
+            cut(str(v)))
 
-def get_vmid(vmids, vmid):
-    success, info = IM_SERVER.GetVMInfo(0, vmid, AUTH)
-    logger.info("GetVMInfo:\n%s" % info)
-    if not success: Exception("Error calling GetVMInfo: %s" % info)
-    if info.strip().startswith("Deleted VM"):
+class spy(object):
+    def __init__(self, C, exception_filter=()): self.C, self.exception_filter = C, exception_filter
+    @staticmethod
+    def log(f, name, exception_filter, *a, **kw):
+        try:
+            r = f(*a, **kw)
+        except exception_filter:
+            logger.exception()
+            return None
+        logger.debug("%s(%s) = %s" % (name, ", ".join(map(san, a) + map(lambda a,b: "%s=%s"%(a, san(b)), kw.items())), san(r)))
+        return r
+    def __getattr__(self, name):
+        f = getattr(self.C, name)
+        return (lambda *a,**kw: spy.log(f, name, self.exception_filter, *a, **kw)) if callable(f) else f
+ 
+IM_SERVER = spy(xmlrpclib.ServerProxy("http://localhost:8899", allow_none=True))
+
+class Control:
+    """Singleton class with basic data and methods."""
+
+    vmids = {} 
+    """dict[hostname]: system"""
+
+    get_meta_state = None
+    """function(): return dict[hostname]: one of 'off', 'idle', 'idle-off', 'busy'."""
+    get_queued_jobs = None
+    """function(): return dict[system ID]: number of systems to deploy."""
+    enable_node = None
+    """function(host, wn): enable host in some class."""
+    disable_node = None
+    """function(host): disable host."""
+
+    @staticmethod
+    def remove_node(h):
+        """function(host): shutdown host."""
+        success, info = IM_SERVER.RemoveResource(0, str(Control.vmids[h].getValue("ec3_vmid")), AUTH)
+        if not success: raise Exception("Error calling RemoveResource: %s" % info)
+        get_vmid(Control.vmids[h].getValue("ec3_vmid"))
+
+    @staticmethod
+    def take_decision():
+        r = {}; s = {}
+        for v in Control.vmids.values():
+            if v.getValue("ec3_meta_state") == "idle": r.setdefault(v.getValue("ec3_class", "wn"), [0])[0] -= 1
+            if v.getValue("ec3_meta_state") == "busy": s.setdefault(v.getValue("ec3_class", "wn"), [0])[0] += 1
+        def add_subs_upto(a, b, c): return a+c, b-c
+        for c, n in Control.get_queued_jobs().items():
+            c0 = c1 = "wn" if LAUNCH_RADL.get(system(c)) is None else c
+            # Compensate an idle node with a job in the queue
+            while c0 and n > 0:
+                r.setdefault(c0, [0])[0], n = add_subs_upto(r.get(c0, [0])[0], n, min(-r.get(c0, [0])[0], n))
+                c0 = LAUNCH_RADL.get(system(c0)).getValue("ec3_if_fail")
+            # If there is too many of one class, it is launch as the class in `ec3_if_fail`
+            while c1 and n > 0:
+                m = LAUNCH_RADL.get(system(c1)).getValue("ec3_max_instances", -1)
+                if m < 0: r.setdefault(c1, [0])[0] += n; break
+                r.setdefault(c1, [0])[0], n = add_subs_upto(r.get(c1, [0])[0], n, min(m-s.get(c1, [0])[0], n))
+                c1 = LAUNCH_RADL.get(system(c1)).getValue("ec3_if_fail")
+        return dict([ (k,v[0]+s.get(k, [0])[0]) for k,v in r.items() ])
+
+def get_vmid(vmid):
+    success, info = IM_SERVER.GetVMInfo(0, str(vmid), AUTH)
+    if not success or info.strip().startswith("Deleted VM") or info.strip().startswith("Invalid VM ID"):
         s0 = system("dummy")
-        for h, v in vmids.items():
+        for h, v in Control.vmids.items():
             if v.getValue("ec3_vmid") == vmid:
                 hostname = h; break
     else: 
         s0 = parse_radl(info).systems[0]
         hostname = s0.getValue("net_interface.0.dns_name")
+        if not hostname:
+            h, hs, _ = socket.gethostbyaddr(s0.getValue("net_interface.0.ip"))
+            hostname = min(map(lambda x: (len(x), x), [h]+hs))[1]
         s0.setValue("ec3_vmid", vmid)
-    s0.setValue("ec3_meta_state", vmids.get(hostname, system("dummy")).getValue("ec3_meta_state"))
-    vmids[hostname] = s0
-    logger.debug("set vmids[%s]=%s" % (hostname, s0))
+    s0.setValue("ec3_meta_state", Control.vmids.get(hostname, system("dummy")).getValue("ec3_meta_state"))
+    Control.vmids[hostname] = s0
 
-def update_meta_state(vmids):
-    for hostname, state in get_meta_state().items():
-        vmids.setdefault(hostname, system("dummy")).setValue("ec3_meta_state", state)
+def get_vmids():
+    success, s = IM_SERVER.GetInfrastructureInfo(0, AUTH)
+    if not success: raise Exception("Error calling GetInfrastructureInfo: %s" % s)
+    Control.vmids.clear()
+    for vmid in s["vm_list"]: get_vmid(vmid)
 
-def take_decision(vmids, queued_jobs):
-    return queued_jobs - len([ 0 for v in vmids.values() if v.getValue("ec3_meta_state") == "idle" ])
+def update_meta_state():
+    for hostname, state in Control.get_meta_state().items():
+        Control.vmids.setdefault(hostname, system("dummy")).setValue("ec3_meta_state", state)
 
-def get_launch_radl(hostname, r):
+def get_launch_radl(hostname, wn, r):
     template = LAUNCH_RADL.clone()
     for n in template.networks: r.add(n, ifpresent="ignore")
-    for s in template.systems:
-        s.name = hostname
-        if s.hasFeature("net_interface.0.dns_name"):
-            s.setValue("net_interface.0.dns_name", hostname)
-        r.add(s)
-    for c in template.configures:
-        c.name = hostname
-        r.add(c)
+    c = template.get(configure(wn))
+    c.name = hostname
+    r.add(c)
+    s = template.get(system(wn))
+    s.name = hostname
+    s.setValue("ec3_class", wn)
+    s.setValue("net_interface.0.dns_name", hostname)
+    r.add(s)
+    r.add(deploy(hostname, 1))
  
-def launch(vmids, n):
-    candidates = []
-    STATES = frozenset(["pending", "running", "configured"])
-    for hostname, v in vmids.items():
-        if not v.getValue("ec3_meta_state"): continue
-        if v.getValue("state") in STATES: n -= 1
-        else: candidates.append(hostname)
-    if not candidates or n <= 0: return
-
-    r = RADL()
+def make_decision(wn, n, r):
+    STATES = frozenset(["pending", "running", "configured", "failed"])
+    for v in Control.vmids.values():
+        if v.getValue("ec3_meta_state") and v.getValue("state") in STATES and v.getValue("ec3_class") == wn: n -= 1
+    if LIMITS[1] is not None: n = max(n, -LIMITS[1])
+    if LIMITS[0] is not None: n = min(n, LIMITS[0])
+    if n < 0: destroy(wn, -n); return
+    candidates = [ h for h, v in Control.vmids.items() if v.getValue("ec3_meta_state") and v.getValue("state") not in STATES ]
     for hostname in candidates[0:n]:
-        enable_node(hostname)
-        get_launch_radl(hostname, r)
-        r.add(deploy(hostname, 1))
-    update_meta_state(vmids)
-    logger.debug("AddResource: %s" % str(r))
+        get_launch_radl(hostname, wn, r)
+        Control.vmids[hostname].setValue("state", "pending")
+        Control.vmids[hostname].setValue("ec3_class", wn)
+
+def launch(r):
+    if not str(r): return
     success, new_vmids = IM_SERVER.AddResource(0, str(r), AUTH)
     if not success: raise Exception("Error calling AddResource: %s" % new_vmids)
-    for vmid in new_vmids:
-        get_vmid(vmids, vmid)
+    for vmid in new_vmids: get_vmid(vmid)
 
-def destroy(vmids, n):
+def destroy(wn, n):
     # Disable idle nodes
-    idle_hostnames = [ hostname for hostname in vmids if vmids[hostname].getValue("ec3_meta_state") == "idle" ]
+    idle_hostnames = [ h for h,v in Control.vmids.items() if v.getValue("ec3_meta_state") == "idle" and
+                                                             v.getValue("ec3_class") == wn ]
     some_change = False
     if DESTROY_INTERVAL:
         t0 = time.time()
-        pair = lambda h: (int(vmids[h].getValue("launch_time", 0) - t0) % DESTROY_INTERVAL, h)
+        pair = lambda h: (int(Control.vmids[h].getValue("launch_time", 0) - t0) % DESTROY_INTERVAL, h)
         for t, hostname in sorted([ pair(hostname) for hostname in idle_hostnames ])[0:n]:
             if 10 < t and t < max(DELAY, 3*60):
-                disable_node(hostname); some_change = True
+                Control.disable_node(hostname); some_change = True
     else:
         for hostname in idle_hostnames[0:n]:
-            disable_node(hostname); some_change = True
-    if some_change: update_meta_state(vmids)
+            Control.disable_node(hostname); some_change = True
+    if some_change: update_meta_state()
 
-def consistency(vmids):
-    def remove_resource(h):
-        logger.info("Destroy %s" % h)
-        success, info = IM_SERVER.RemoveResource(0, str(vmids[h].getValue("ec3_vmid")), AUTH)
-        if not success: raise Exception("Error calling RemoveResource: %s" % info)
-        get_vmid(vmids, vmids[h].getValue("ec3_vmid"))
+def consistency():
     INCONSISTENT_STATES = (
-        #(frozenset(["off", "pending", "running"]), frozenset(["idle", "busy"]), lambda h: disable_node(h)),
-        (frozenset(["pending", "running", "configured"]), frozenset(["idle-off"]), remove_resource),
-        (frozenset(["failed"]), frozenset(["off", "idle", "idle-off", "busy"]), remove_resource))
-    for hostname, v in vmids.items():
-        for states, meta_states, _ in INCONSISTENT_STATES:
-            logger.debug("consistency %s %s %s" % (hostname, v.getValue("state") in states, v.getValue("ec3_meta_state") in meta_states))
-            if v.getValue("state") in states and v.getValue("ec3_meta_state") in meta_states:
-                get_vmid(vmids, v.getValue("ec3_vmid")); v = vmids[hostname]; break
+        (frozenset(["configured"]), frozenset(["off"]), Control.enable_node),
+        (frozenset(["off"]), frozenset(["idle", "busy"]), Control.disable_node),
+        (frozenset(["pending", "running", "configured"]), frozenset(["idle-off"]), Control.remove_node)
+        (frozenset(["failed"]), frozenset(["off", "idle", "idle-off", "busy"]), Control.remove_node))
+    for hostname, v in Control.vmids.items():
         for states, meta_states, f in INCONSISTENT_STATES:
-            if v.getValue("state") in states and v.getValue("ec3_meta_state") in meta_states:
+            if v.getValue("state", "off") in states and v.getValue("ec3_meta_state") in meta_states:
                 f(hostname)
 
-def loop_body(vmids):
-    # Collect information about jobs
-    queued_jobs = get_queued_jobs()
-    logger.debug("queued_jobs = %d" % queued_jobs)
-
+def loop_body():
     # Collect information about nodes
-    update_meta_state(vmids)
-    logger.debug("vmids = %s" % str(dict([ (h, v.__str__()) for h,v in vmids.items() ])))
+    get_vmids()
+    update_meta_state()
+    logger.debug("vmids = %s" % san(Control.vmids, 1000))
 
-    # Take decision
-    new_nodes = take_decision(vmids, queued_jobs)
-    new_nodes = max(new_nodes, -LIMITS[1]) if LIMITS[1] is not None else new_nodes
-    new_nodes = min(new_nodes, LIMITS[0]) if LIMITS[0] is not None else new_nodes
-    logger.debug("decision: %d launch/destroy" % new_nodes)
-
-    # Apply correction
-    if new_nodes > 0: launch(vmids, new_nodes)
-    elif new_nodes < 0: destroy(vmids, -new_nodes)
-    consistency(vmids)
+    # Take decision and apply correction
+    r = RADL()
+    for wn, new_nodes in Control.take_decision().items(): make_decision(wn, new_nodes, r)
+    launch(r)
+    consistency()
 
 def loop_cmd(seconds, cmd):
     if isinstance(cmd, list): cmd = " ".join(cmd)
@@ -157,7 +200,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-a", "--auth", dest="auth", nargs=1, type=argparse.FileType('r'), help="Authorization file")
     parser.add_argument("-r", "--radl", dest="radl", nargs=1, type=argparse.FileType('r'), help="RADL working nodes system content")
-    parser.add_argument("-w", "--wrapper", dest="wrapper", nargs=1, type=argparse.FileType('r'), help="Python file to get queued jobs and nodes state")
+    parser.add_argument("-w", "--wrapper", dest="wrapper", nargs="+", type=argparse.FileType('r'), help="Python file to get queued jobs and nodes state")
     parser.add_argument("-l", "--log-file", dest="log_file", nargs=1, type=argparse.FileType('w'), default=[sys.stderr], help="Log output file")
     parser.add_argument("-ll", "--log-level", dest="log_level", nargs=1, type=int, default=[4], help="1: debug; 2: info; 3: warning; 4: error")
     parser.add_argument("-d", "--delay", dest="delay", nargs=1, type=int, default=[20], help="the cluster is checked every this number of seconds")
@@ -185,17 +228,14 @@ if __name__ == "__main__":
     except Exception, e:
         logger.error("Error in -r/--radl: %s\n" % str(e))
         sys.exit(1)
-    try:
-        if not options.wrapper: raise Exception("option is not set")
-        description = [ d for d in imp.get_suffixes() if d[0] == ".py" ][0]
-        wrapper = imp.load_module("wrapper", options.wrapper[0], options.wrapper[0].name, description)
-        get_meta_state = wrapper.get_meta_state
-        get_queued_jobs = wrapper.get_queued_jobs
-        enable_node = wrapper.enable_node
-        disable_node = wrapper.disable_node
-    except Exception, e:
-        logger.error("Error in -w/--wrapper: %s\n" % str(e))
-        sys.exit(1)
+    description = [ d for d in imp.get_suffixes() if d[0] == ".py" ][0]
+    for opt_wrapper in options.wrapper:
+        try:
+            imp.load_module("wrapper", opt_wrapper, opt_wrapper.name, description).ec3_init(Control)
+        except Exception, e:
+            logger.error("Error in -w/--wrapper in '%s': %s\n" % (opt_wrapper.name, str(e)))
+            sys.exit(1)
+    Control = spy(Control, (subprocess.CalledProcessError,))
     DELAY = options.delay[0]
     LIMITS = (options.limit_create[0], options.limit_destroy[0])
     DESTROY_INTERVAL = options.destroy_interval[0]
@@ -212,16 +252,11 @@ if __name__ == "__main__":
         t.daemon = True
         t.start()
     try:
-        vmids = {}
         while True:
             time0 = time.time()
             logger.info("New iteration")
-            try:
-                if not vmids:
-                    get_vmids(vmids)
-                loop_body(vmids)
-            except:
-                logger.exception("Exception in loop:")
+            try: loop_body()
+            except: logger.exception("Exception in loop:")
             time.sleep(max(DELAY - (time.time() - time0), 0))
     except KeyboardInterrupt:
         logger.info("Program interrupted by the user.")
