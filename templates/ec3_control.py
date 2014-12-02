a0c1a7fe6057071e26f67008fbe42df26d002c1b
Eloy Romero <elroal@upvnet.upv.es>
2014-12-02 13:10:40 +0100

    Improved support to ec3_{max,min}_instances and added ec3_max_{creation,remove}.

--- templates/ec3_control.py
+++ templates/ec3_control.py
@@ -7,8 +7,9 @@ from IM.auth import Authentication
 from IM.radl.radl_parse import parse_radl
 from IM.radl.radl import RADL, system, configure, deploy
 
-AUTH = LAUNCH_RADL = LIMITS = DESTROY_INTERVAL = DELAY = None
+AUTH = LAUNCH_RADL = DELAY = None
 logger = None
+FAILED = {} # dict from ec3_class to tuple of freedom date and penalty time.
 
 def san(v, maxlen=600):
     cut = lambda r: r if len(r) < maxlen else r[0:maxlen-6] + "..." + r[-3:-1]
@@ -24,7 +25,7 @@ class spy(object):
         try:
             r = f(*a, **kw)
         except exception_filter:
-            logger.exception()
+            logger.exception("%s(%s)" % (name, ", ".join(map(san, a) + map(lambda a,b: "%s=%s"%(a, san(b)), kw.items()))))
             return None
         logger.debug("%s(%s) = %s" % (name, ", ".join(map(san, a) + map(lambda a,b: "%s=%s"%(a, san(b)), kw.items())), san(r)))
         return r
@@ -58,25 +59,16 @@ class Control:
 
     @staticmethod
     def take_decision():
-        r = {}; s = {}
+        # Nodes = Queued + Busy
+        r = {}
         for v in Control.vmids.values():
-            if v.getValue("ec3_meta_state") == "idle": r.setdefault(v.getValue("ec3_class", "wn"), [0])[0] -= 1
-            if v.getValue("ec3_meta_state") == "busy": s.setdefault(v.getValue("ec3_class", "wn"), [0])[0] += 1
-        def add_subs_upto(a, b, c): return a+c, b-c
+            if v.getValue("ec3_meta_state") == "busy": r.setdefault(v.getValue("ec3_class", "wn"), [0])[0] += 1
         for c, n in Control.get_queued_jobs().items():
-            c0 = c1 = "wn" if LAUNCH_RADL.get(system(c)) is None else c
-            # Compensate an idle node with a job in the queue
-            while c0 and n > 0:
-                r.setdefault(c0, [0])[0], n = add_subs_upto(r.get(c0, [0])[0], n, min(-r.get(c0, [0])[0], n))
-                c0 = LAUNCH_RADL.get(system(c0)).getValue("ec3_if_fail")
-            # If there is too many of one class, it is launch as the class in `ec3_if_fail`
-            while c1 and n > 0:
-                m = LAUNCH_RADL.get(system(c1)).getValue("ec3_max_instances", -1)
-                if m < 0: r.setdefault(c1, [0])[0] += n; break
-                r.setdefault(c1, [0])[0], n = add_subs_upto(r.get(c1, [0])[0], n, min(m-s.get(c1, [0])[0], n))
-                c1 = LAUNCH_RADL.get(system(c1)).getValue("ec3_if_fail")
-        return dict([ (k,v[0]+s.get(k, [0])[0]) for k,v in r.items() ])
-
+            r.setdefault("wn" if LAUNCH_RADL.get(system(c)) is None else c, [0])[0] += n
+        for s in LAUNCH_RADL.systems:
+            if s.getId() != "front": r.setdefault(s.getId(), [0])
+        return dict([ (k,v[0]) for k,v in r.items() ])
+        
 def get_vmid(vmid):
     vmid = str(vmid) # IM bug: some IM calls return an integer as VM id
     success, info = IM_SERVER.GetVMInfo(0, vmid, AUTH)
@@ -118,52 +110,81 @@ def get_launch_radl(hostname, wn, r):
     s.setValue("net_interface.0.dns_name", hostname)
     r.add(s)
     r.add(deploy(hostname, 1))
+
+def timeline(v):
+    """ Return a value so that the less the more change to survive. """
+
+    def time_up_to_deadline(v):
+        if v.getValue("ec3_meta_state") in frozenset(["idle-off", "busy-off"]):
+            return None   #NOTE: None <= anything
+        if v.getValue("ec3_meta_state") != "idle":
+            return 1e9
+        if v.getValue("launch_time") and v.getValue("ec3_destroy_interval", 0) > 60:
+            return int(v.getValue("launch_time") - time.time()) % int(v.getValue("ec3_destroy_interval"))
+        return int(v.getValue("ec3_max_idle_time", 0) - time.time() + v.getValue("idle_time", time.time()))
+    MSTATE = dict([ (a,b) for b,a in enumerate(("busy", "idle", "busy-off", "idle-off", "off")) ])
+    STATE = dict([ (a,b) for b,a in enumerate(("configured", "running", "pending", "failed", "off")) ])
+    return MSTATE[v.getValue("ec3_meta_state")], STATE.get(v.getValue("state"), 9), time_up_to_deadline(v)
  
-def make_decision(wn, n, r):
-    STATES = frozenset(["pending", "running", "configured", "failed"])
-    for v in Control.vmids.values():
-        if v.getValue("ec3_meta_state") and v.getValue("state") in STATES and v.getValue("ec3_class") == wn: n -= 1
-    if LIMITS[1] is not None: n = max(n, -LIMITS[1])
-    if LIMITS[0] is not None: n = min(n, LIMITS[0])
-    if n < 0: destroy(wn, -n); return
-    candidates = [ h for h, v in Control.vmids.items() if v.getValue("ec3_meta_state") and v.getValue("state") not in STATES ]
-    for hostname in candidates[0:n]:
-        get_launch_radl(hostname, wn, r)
-        Control.vmids[hostname].setValue("state", "pending")
-        Control.vmids[hostname].setValue("ec3_class", wn)
+def make_decision(wn, n, r, limits, force=False, create=True, destroy=True):
+    card = {}
+    for c in [ v.getValue("ec3_class") for v in Control.vmids.values() if v.getValue("ec3_class") in frozenset(wn) ]:
+        card[c] = card.get(c, 0) + 1
+    change = False
+    nodes = [ (timeline(v), h, v) for h, v in Control.vmids.items() if v.getValue("ec3_class") in frozenset(wn) or
+              (v.getValue("ec3_meta_state", "off") == "off" and v.getValue("state", "off") == "off") ]
+    for t, h, v in sorted(nodes):
+        if n > 0 and create:
+            if (v.getValue("ec3_meta_state") in frozenset(["busy-off", "idle-off"]) or
+                    (v.getValue("state") == "configured" and v.getValue("ec3_meta_state") == "off")):
+                Control.enable_node(h); change = True
+            elif v.getValue("state", "off") == "off":
+                c = launch_radl(wn, h, r, card, limits)
+                if c: card[c] = card.get(c, 0) + 1; limits[c][0] -= 1
+        elif n < 1 and destroy:
+            if (v.getValue("ec3_meta_state") in frozenset(["idle", "busy"]) and limits[v.getValue("ec3_class")][1] > 0
+                    and (force or t[2] >= v.getValue("ec3_destroy_safe", None))):
+                Control.disable_node(h); limits[v.getValue("ec3_class")][1] -= 1; change = True
+            elif (v.getValue("ec3_meta_state") in frozenset(["idle-off", "off"]) and
+                    v.getValue("state", "off") not in frozenset(["running", "off"]) and
+                    v.getValue("ec3_min_instances", 0) < card[v.getValue("ec3_class")]):
+                c = v.getValue("ec3_class"); Control.remove_node(h); card[c] -= 1
+        n -= 1
+    if change: update_meta_state()
+
+def launch_radl(wn, hostname, r, card, limits):
+    for c in wn + [None]:
+        if c: m = int(LAUNCH_RADL.get(system(c)).getValue("ec3_max_instances", -1))
+        if c and (time.time() >= FAILED.get(c, (0,))[0]) and (m < 0 or card.get(c, 0) < m) and limits[c][0] > 0: break
+    if not c: return None
+    # Workaround IM bug: force to launch all systems of the same kind to identify clearly which class failed
+    if r.systems and r.systems[0].getValue("ec3_class") != c: return None
+    get_launch_radl(hostname, c, r)
+    Control.vmids[hostname].setValue("state", "pending")
+    Control.vmids[hostname].setValue("ec3_class", c)
+    return c
 
 def launch(r):
     if not str(r): return
     success, new_vmids = IM_SERVER.AddResource(0, str(r), AUTH)
-    if not success: raise Exception("Error calling AddResource: %s" % new_vmids)
-    for vmid in new_vmids: get_vmid(vmid)
+    if not success and new_vmids.find("Error allocating a new virtual machine") >= 0:
+        for s in set([ s.getValue("ec3_class") for s in r.systems ]):
+            penalty = min(FAILED.get(s, (None, DELAY))[1]*2, 1e7)
+            FAILED[s] = (time.time()+penalty, penalty)
+    if success:
+        for s in set([ s.getValue("ec3_class") for s in r.systems ]):
+            if s in FAILED: FAILED[s][1] = min(1, FAILED[s][1]/2)
+        for vmid in new_vmids: get_vmid(vmid)
 
-def destroy(wn, n):
-    # Disable idle nodes
-    idle_hostnames = [ h for h,v in Control.vmids.items() if v.getValue("ec3_meta_state") == "idle" and
-                                                             v.getValue("ec3_class") == wn ]
-    some_change = False
-    if DESTROY_INTERVAL:
-        t0 = time.time()
-        pair = lambda h: (int(Control.vmids[h].getValue("launch_time", 0) - t0) % DESTROY_INTERVAL, h)
-        for t, hostname in sorted([ pair(hostname) for hostname in idle_hostnames ])[0:n]:
-            if 10 < t and t < max(DELAY, 3*60):
-                Control.disable_node(hostname); some_change = True
-    else:
-        for hostname in idle_hostnames[0:n]:
-            Control.disable_node(hostname); some_change = True
-    if some_change: update_meta_state()
-
-def consistency():
-    INCONSISTENT_STATES = (
-        (frozenset(["configured"]), frozenset(["off"]), Control.enable_node),
-        (frozenset(["off"]), frozenset(["idle", "busy"]), Control.disable_node),
-        (frozenset(["pending", "running", "configured"]), frozenset(["idle-off"]), Control.remove_node),
-        (frozenset(["failed"]), frozenset(["off", "idle", "idle-off", "busy"]), Control.remove_node))
-    for hostname, v in Control.vmids.items():
-        for states, meta_states, f in INCONSISTENT_STATES:
-            if v.getValue("state", "off") in states and v.getValue("ec3_meta_state") in meta_states:
-                f(hostname)
+def toposort(classes):
+    v, path = {}, {}
+    for c in classes:
+        path[c] = visited = []; n = 0
+        while c and c not in visited:
+            v[c] = max(v.get(c, n), n)
+            visited.append(c)
+            c, n = LAUNCH_RADL.get(system(c)).getValue("ec3_if_fail"), n+1
+    return map(lambda k: path[k[1]], sorted([ (-v[c], c) for c in classes ]))
 
 def loop_body():
     # Collect information about nodes
@@ -171,11 +192,32 @@ def loop_body():
     update_meta_state()
     logger.debug("vmids = %s" % san(Control.vmids, 1000))
 
+    # Failure control
+    if any([ v.getId() == "front" and v.getValue("state") == "failed" for v in Control.vmids.values() ]):
+        IM_SERVER.Reconfigure(0, "", AUTH); return
+    for h, v in Control.vmids.items():
+        if v.getValue("state") == "failed": Control.remove_node(h)
+
+    # Compute limits on every class
+    limits = dict([ (s.getId(), [s.getValue("ec3_max_creation", 1e7)*DELAY,
+                                 s.getValue("ec3_max_remove", 1e7)*DELAY]) for s in LAUNCH_RADL.systems ])
+    
     # Take decision and apply correction
     r = RADL()
-    for wn, new_nodes in Control.take_decision().items(): make_decision(wn, new_nodes, r)
+    decision = Control.take_decision()
+    for v in LAUNCH_RADL.systems:
+        if v.getId() == "front": continue
+        decision.setdefault(v.getId(), 0)
+        make_decision([v.getId()], int(v.getValue("ec3_min_instances", 0)), r, limits, destroy=False)
+        if int(v.getValue("ec3_max_instances", -1)) > 0:
+            make_decision([v.getId()], int(v.getValue("ec3_max_instances")), r, limits, force=True, create=False)
+    for path in toposort(decision.keys()):
+        decision_min = decision_max = 0
+        for v in [ LAUNCH_RADL.get(system(c)) for c in path ]:
+            decision_min += int(v.getValue("ec3_min_instances", 0))
+            decision_max += int(v.getValue("ec3_max_instances")) if int(v.getValue("ec3_max_instances", -1)) > 0 else 1e7
+        make_decision(path, min(max(sum([ decision.get(c, 0) for c in path ]), decision_min), decision_max), r, limits)
     launch(r)
-    consistency()
 
 def loop_cmd(seconds, cmd):
     if isinstance(cmd, list): cmd = " ".join(cmd)
@@ -206,10 +248,6 @@ if __name__ == "__main__":
     parser.add_argument("-l", "--log-file", dest="log_file", nargs=1, type=argparse.FileType('w'), default=[sys.stderr], help="Log output file")
     parser.add_argument("-ll", "--log-level", dest="log_level", nargs=1, type=int, default=[4], help="1: debug; 2: info; 3: warning; 4: error")
     parser.add_argument("-d", "--delay", dest="delay", nargs=1, type=int, default=[20], help="the cluster is checked every this number of seconds")
-    parser.add_argument("-ld", "--limit-destroy", dest="limit_destroy", nargs=1, type=int_none, default=[None], help="maximum machines to destroy at once")
-    parser.add_argument("-lc", "--limit-create", dest="limit_create", nargs=1, type=int_none, default=[None], help="maximum machines to launch at once")
-    parser.add_argument("-di", "--destroy-interval", dest="destroy_interval", nargs=1, type=mins, default=[0],
-                        help="destroy machines only when they are at the end of this interval")
     parser.add_argument("-c", "--command", dest="cmds", action="append", nargs="+", help="execute command frequently", default=[])
     parser.add_argument("-tc", "--timed-command", dest="timed_cmds", action="append", nargs="+", help="execute command every the number of seconds passed as the first item", default=[])
     options = parser.parse_args()
@@ -239,8 +277,6 @@ if __name__ == "__main__":
             sys.exit(1)
     Control = spy(Control, (subprocess.CalledProcessError,))
     DELAY = options.delay[0]
-    LIMITS = (options.limit_create[0], options.limit_destroy[0])
-    DESTROY_INTERVAL = options.destroy_interval[0]
     commands = [ (DELAY, c) for c in options.cmds ]
     try:
         for c in options.timed_cmds:
