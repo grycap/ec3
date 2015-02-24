8a7a3fe9391a7372b98004a9557f752f94c3b1b0
micafer <micafer1@upv.es>
2015-02-24 17:28:02 +0100

    Solve some conflicts with the first commit of this branch

--- IM2/radl/radl.py
+++ IM2/radl/radl.py
@@ -16,7 +16,12 @@
 
 import copy
 import itertools
-from urlparse import urlparse
+try:
+	from urlparse import urlparse
+	le2 = lambda a,b: a <= b
+except ImportError:
+	from urllib.parse import urlparse
+	le2 = lambda a,b: True if a is None else False if b is None else a <= b
 import heapq as heap
 
 def UnitToValue(unit):
@@ -97,7 +102,7 @@ def _combinations(c):
 			heap.heappush(combs, (ci1[0]-c[i][0][0]+score0, c1))
 
 
-class Feature:
+class Feature(object):
 	"""
 	Every property that can appear in the definitions of a ``network`` and ``system``.
 
@@ -149,7 +154,7 @@ class Feature:
 		if unit or self.unit:
 			try:
 				r = float(self.value * UnitToValue(self.unit)) / UnitToValue(unit)
-			except Exception, e:
+			except Exception as e:
 				raise RADLParseException(str(e), line=self.line)
 			return int(round(r)) if isinstance(self.value, int) else r
 		return self.value
@@ -344,7 +349,7 @@ class Features(object):
 			inter0 = self.props.get(f.prop, (None, None))
 			try:
 				self.props[f.prop] = Features._applyInter(inter0, inter1, conflict)
-			except Exception, e:
+			except Exception as e:
 				raise RADLConflict(str(e), f0=inter0, f1=f)
 		elif isinstance(f, SoftFeatures):
 			self.props.setdefault(f.prop, set()).add(f)
@@ -360,7 +365,7 @@ class Features(object):
 				raise RADLConflict("Conflict adding `%s` because `%s` is not a set, it is '%s'." % (f, f.prop, self.props[f.prop]))
 		elif isinstance(f.value, Aspect):
 			value0 = self.props.get(f.prop, None)
-			if not value0 or not value0.value:
+			if not value0 or not value0.value or value0.value.getId() != f.value.getId():
 				self.props[f.prop] = f
 			else:
 				self.props[f.prop].value.merge(f.value, conflict=conflict, missing=missing)
@@ -461,12 +466,12 @@ class Features(object):
 		# Remember, None <= number and None <= None are True, but number <= None is False.
 		inter0 = tuple([f.getValue() if f else None for f in finter0])
 		inter1 = tuple([f.getValue() if f else None for f in finter1])
-		le00 = inter0[0] <= inter1[0]                         # finter0[0] <= finter1[0]
-		le01 = inter1[1] == None or inter0[0] <= inter1[1]    # finter0[0] <= finter1[1]
-		le11 = inter1[1] == None or (inter0[1] != None and inter0[1] <= inter1[1])
+		le00 = le2(inter0[0], inter1[0])                      # finter0[0] <= finter1[0]
+		le01 = inter1[1] == None or le2(inter0[0], inter1[1]) # finter0[0] <= finter1[1]
+		le11 = inter1[1] == None or (inter0[1] != None and le2(inter0[1], inter1[1]))
 		                                                      # finter0[1] <= finter1[1]
 		ge00 = not le00 or inter0[0] == inter1[0]             # finter0[0] >= finter1[0]
-		ge10 = inter0[1] == None or inter0[1] >= inter1[0]    # finter0[1] >= finter1[0]
+		ge10 = inter0[1] == None or le2(inter1[0], inter0[1]) # finter0[1] >= finter1[0]
 
 		# First interval is (  ), second interval is [  ]
 		if le00 and ge10 and le11:                       # ( [ ) ] chain first-second
@@ -611,7 +616,7 @@ class Aspect:
 		aspect0 = aspect0.clone()
 		try:
 			aspect0.merge(aspect1, **kargs)
-		except RADLConflict, e:
+		except RADLConflict as e:
 			if iferror == "error":
 				raise e
 			else:
@@ -1088,6 +1093,9 @@ class SoftFeatures(_system, Feature):
 		self.line = line
 		_system.__init__(self, features)
 		Feature.__init__(self, SoftFeatures.SOFT, "contains", self)
+
+	def __hash__(self):
+		return _system.__hash__(self)
 
 	def __eq__(self, other):
 		return isinstance(other, SoftFeatures) and Features.__eq__(self, other)
