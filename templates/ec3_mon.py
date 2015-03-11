00c1daa6a4388b9a912fbd350bc0bc61eef30b8a
Amanda Calatrava <acalatrava46@gmail.com>
2015-03-11 13:14:35 +0100

    Temporally disable spot price information

--- templates/ec3_mon.py
+++ templates/ec3_mon.py
@@ -38,9 +38,10 @@ def get_price(s, auth):
     return None
 
 def on_new_iteration(old_func):
-    for v in Control_ref[0].vmids.values():
-        if v.getValue("spot", "no") != "yes": continue
-        v.setValue("price", get_price(v, Control_ref[0].AUTH))
+    # Temporally disable spot price information
+    #for v in Control_ref[0].vmids.values():
+        #if v.getValue("spot", "no") != "yes": continue
+        #v.setValue("price", get_price(v, Control_ref[0].AUTH))
     f = open('mon.txt', 'a')
     d = dict([ (a,str(b)) for a,b in Control_ref[0].vmids.items() ])
     d["time"] = time.time()
