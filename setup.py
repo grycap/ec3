882c2c25af894ee2375eef7acf369ee57d3247f8
micafer <micafer1@upv.es>
2015-05-19 18:14:21 +0200

    Add the templates in the setup

--- setup.py
+++ setup.py
@@ -3,6 +3,11 @@
 import os
 from setuptools import setup
 
+# Add contextualization dir files
+install_path = '/etc/ec3/'
+datafiles = [(os.path.join(install_path, root), [os.path.join(root, f) for f in files])
+    for root, dirs, files in os.walk("templates")]
+
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
 # README file and 2) it's easier to type in the README file than to put a raw
@@ -14,11 +19,12 @@ setup(
     name = "ec3",
     version = "0.0.1",
     author = "Eloy Romero, Amanda Calatrava",
-    author_email = "",
+    author_email = "elroal@pv.es, amcaar@upv.es",
     description = ("Tool to deploy elastic clusters on cloud."),
     license = "GPL",
     keywords = "cloud cluster elasticity",
     url = "http://www.grycap.upv.es/ec3/",
+	data_files=datafiles,
     packages=['IM2', 'IM2.radl', 'IM2.radl.ply'],
     package_data={'IM2.radl': ['radl_schema.json']},
     scripts=["ec3"],
